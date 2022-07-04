50757e4a6dbd2f5c0b8cebdcd8c0193004403473
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-07-04 18:17:36 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-// Create Pixel Board ---------------------------------|
-const pixelBoard = document.querySelector('#pixel-board');
+let selectedColor = getDefaultColor();
+
+// ---------------------------------------------------------------|
 
 function createPixels() {
+  const pixelBoard = document.querySelector('#pixel-board');
   const pixel = document.createElement('div');
   pixel.className = 'pixel';
+
+  pixel.addEventListener('click', () => {
+    pixel.style.backgroundColor = selectedColor;
+  });
+
   pixelBoard.appendChild(pixel);
 }
 
 for (let i = 0; i < 25; i++) createPixels();
+
+// ---------------------------------------------------------------|
+
+// Handle Color Palette
+function handleColorPalette() {
+  const colors = document.querySelectorAll('.color');
+
+  for (const color of colors) {
+    color.addEventListener('click', () => {
+      selectedColor = window.getComputedStyle(color, null).backgroundColor;
+
+      for (const color of colors) {
+        if (color.classList.contains('selected')) {
+          color.classList.remove('selected');
+        }
+      }
+
+      color.classList.add('selected');
+    });
+  }
+}
+
+handleColorPalette();
+
+// ---------------------------------------------------------------|
+
+function getDefaultColor() {
+  let defaultColor = document.querySelector('.selected');
+  defaultColor = window.getComputedStyle(defaultColor, null).backgroundColor;
+
+  return defaultColor;
+}
